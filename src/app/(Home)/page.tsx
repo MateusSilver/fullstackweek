fead1fe0829393c3b41929f8e1a537f22919f876
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-11-12 22:26:32 -0300

    feat: added mouses product list

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -23,6 +23,14 @@ export default async function Home() {
     }
   });
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses"
+      }
+    }
+  });
+
   return <div className="flex flex-col gap-12">
     <PromoBanner 
       src={"/banner-home-01.png"} 
@@ -48,10 +56,16 @@ export default async function Home() {
       <SessionTitle>Teclados</SessionTitle>
       <ProductList products={keyboards}/>
     </div>
+
     <PromoBanner 
       src={"/banner-home-03.png"} 
       alt={"Até 20% de desconto em fones"}
     />
+
+    <div>
+      <SessionTitle>Mouses</SessionTitle>
+      <ProductList products={mouses}/>
+    </div>
 
   </div>;
 }
