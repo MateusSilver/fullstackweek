c416c8203fa1a9f2388f33efe0c42c8803b14869
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-11-18 21:53:15 -0300

    fix: ensure category button in home page goes to category page

--- src/app/(Home)/components/category-item.tsx
+++ src/app/(Home)/components/category-item.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { CATEGORY_ICON } from "@/constants/category-icon";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CategoryItemProps {
     category: Category;
@@ -8,10 +9,13 @@ interface CategoryItemProps {
 
 const CategoryItem = ({category}:CategoryItemProps) => {
     return ( 
-        <div className="flex items-center justify-center border-solid border-2 gap-2 rounded-lg py-3">
-            {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-            <span className="text-xs font-bold">{category.name}</span>
-        </div>
+        <Link href={`/category/${category.slug}`}>
+            <div className="flex items-center justify-center border-solid border-2 gap-2 rounded-lg py-3">
+                {CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+                <span className="text-xs font-bold">{category.name}</span>
+            </div>
+        </Link>
+        
     );
 }
 
